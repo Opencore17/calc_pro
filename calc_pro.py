5b835b7c1645d2cdc61b4d279c6dcf6d2957a19f
Ronan <79341006+Opencore17@users.noreply.github.com>
2022-04-06 17:04:38 +0100

    Update and rename quad_calc.py to calc_pro.py

--- calc_pro.py
+++ calc_pro.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+
 import cmath
 import matplotlib.pyplot as plt
 
 def quadratic_solver():
-    a = float(input("A? "))
-    b = float(input("B? "))
-    c = float(input("C? "))
     while True:
-            switch = int(input("Function=0 Quadratics=1 Main Menu=2 "))
-            if switch == 0:
+            switch1 = int(input("Function=0 Quadratics=1 Main Menu=2 "))
+            if switch1 == 0:
+                a = float(input("A? "))
+                b = float(input("B? "))
+                c = float(input("C? "))
                 x = []
                 y = []
                 #l = lowest number
@@ -33,7 +35,7 @@ def quadratic_solver():
                         pass
                     l = l + 1
             
-            elif switch == 1:
+            elif switch1 == 1:
                 a = float(input("A? "))
                 b = float(input("B? "))
                 c = float(input("C? "))
@@ -44,26 +46,53 @@ def quadratic_solver():
 
                 print("The solutions are {0} and {1}".format(s1,s2))
             
-            elif switch ==2:
+            elif switch1 ==2:
                 break
             else:
                 pass
+
+def DST_solver():
+    while True:
+        switch2 = int(input("Distance=0 Speed=1 Time=2 Main Menu=3 "))
+        if switch2 == 0:
+            s0 = float(input("Speed? "))
+            t0 = float(input("Time?(Decimal Format) "))
+            d0 = s0*t0
+            print("Distance is " + str(d0))
+        elif switch2 == 1:
+            d1 = float(input("Distance? "))
+            t1 = float(input("Time?(Decimal Format) "))
+            s1 = d1/t1
+            print("Speed is " + str(s1))
+        elif switch2 == 2:
+            d2 = float(input("Distance? "))
+            s2 = float(input("Speed? "))
+            t2 = d2/s2
+            print("Time is " + str(t2))
+        elif switch2 == 3:
+            break
+        else:
+            pass
 
 
 graph = 1
 values = 1
 while True:
 
-    mode = int(input("What mode? Quadratics=0 Settings=1 End=2 "))
+    mode = int(input("What mode? Quadratics=0 Distance/Speed/Time=1 Settings=2 End=3 "))
 
     if mode == 0:
         quadratic_solver()
 
-    if mode == 1:
+    elif mode == 1:
+        DST_solver()
+
+    elif mode == 2:
         print("SETTINGS")
         graph = int(input("Show graph? 1 = Yes "))
         values = int(input("Show values? 1 = Yes "))
-    elif mode == 2:
+
+    elif mode == 3:
         exit()
 
     else:
